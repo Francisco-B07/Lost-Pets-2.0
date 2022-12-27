c904e7324d058a0cb1c694bc2f02d67e7aec4a1e
Francisco Burgoa <franciscojburgoa@gmail.com>
2022-12-27 15:09:51 -0300

    corregí archivo router/index

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "pages/home";
-import { Page2 } from "pages/Page2";
 import { Layout } from "components/layout";
 import { Search } from "pages/Search";
 import { SearchResults } from "pages/SearchResults";
